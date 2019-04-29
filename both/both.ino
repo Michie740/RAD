b3b6f60f437d6d76d266990a4e336eebf7aacd8e
aboblin <aob270@nyu.edu>
2019-04-29 19:29:11 -0400

    Update both.ino
    did the things w the lats n lngs

--- both/both.ino
+++ both/both.ino
@@ -1,4 +1,23 @@
 #include <elapsedMillis.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+#define R 6371
+#define TO_RAD (3.1415926536 / 180)
+
+
+  double dist(double th1, double ph1, double th2, double ph2) {
+  double dx, dy, dz;
+  ph1 -= ph2;
+  ph1 *= TO_RAD, th1 *= TO_RAD, th2 *= TO_RAD;
+ 
+  dz = sin(th1) - sin(th2);
+  dx = cos(ph1) * cos(th1) - cos(th2);
+  dy = sin(ph1) * cos(th1);
+  return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
+  }
+
 int ledleft = 12;
 int ledright = 8;
 int state = 0;
@@ -28,6 +47,7 @@ void loop() {
     }
     else{
       digitalWrite(ledPin, HIGH);
+      Serial.write("2");
     }
   }
   else{  
@@ -39,6 +59,24 @@ void loop() {
   if (Serial.available() > 0){ // if there is data
     state = Serial.read();
   }
+  char myString[] = '';
+  double coordinates[4];
+
+  if (state.length() > 1){
+
+  myString = strtok( state, ",\n");
+  
+  for (int i=0; i<4; i++){
+    coordinates[i]= myString;
+    myString = strtok(NULL, ",\n");
+  }
+
+  if (dist(coordinates[0], coordinates[2], coordinates[1], coordinates[3]) <76.2){
+    Serial.write("0");
+    delay(5000);
+    Serial.write("1");
+  }
+}
   if (state == 0){
     digitalWrite(ledleft, LOW);
     digitalWrite(ledright, LOW);
